dff0492e0e07d6cb74303cde0c25515230dbd127
Hemanth Somesula <hemanth.somesula@500apps.com>
2020-09-20 11:43:47 +0530

    Basic code

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+     <button v-on:click="recognize">recognize</button>
+    <img id="text-img" alt="Vue logo" src="./assets/testocr.png">
+    {{this.text}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+/* eslint-disable */
+import { createWorker, PSM, OEM } from "tesseract.js";
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
+
+export default {
+  name: "App",
+  data(){
+    return {
+      text:''
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+
+  },
+
+  methods: {
+    recognize: async () => {
+      let that = this;
+      const img = document.getElementById("text-img");
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng", OEM.LSTM_ONLY);
+      await worker.setParameters({
+        tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+      });
+      const {
+        data: { text },
+      } = await worker.recognize(img);
+      that.text = text;
+      console.log(text);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ImageReader.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+</template>
+
+<script>
+import { createWorker } from 'tesseract.js';
+export default {
+  name: 'ImageReader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
