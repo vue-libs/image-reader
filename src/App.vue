87ea1c40a6b30e90c770ac69bffa84135ee4c4f3
tabrezdn1 <tabrezdn1@gmail.com>
2021-05-04 03:42:03 +0530

    Update main page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <div class="container">
-    <ImageReader v-model="text" :config="config" />
-    This is text: {{ text }}
+  <div class="container-fluid">
+    <div class="header">
+      <nav class="navbar navbar-light bg-light">
+        <a
+          class="navbar-brand"
+          href="https://github.com/vue-libs/image-reader"
+          target="_blank"
+        >
+          <img
+            src="https://avatars.githubusercontent.com/u/37013844?v=4"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          Image Reader DEMO : Made using tesseract JS in VUE
+        </a>
+      </nav>
+    </div>
+    <b-card>
+      <div>
+        <ImageReader v-model="text" :config="config" />
+        <b-card
+          bg-variant="light"
+          header="This is the converted text"
+          class="text-center"
+        >
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card>
+      </div>
+    </b-card>
   </div>
 </template>
 
